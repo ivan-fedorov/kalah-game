9b56ce77174574c0e7f7e241c62fcab839a459c7
Roman Golovin <roman_gr@mail.com>
2021-08-08 18:39:12 +0300

    Extract game joining logic and make it state-independent

--- src/main/java/com/fivan/kalah/bot/handler/InitialStateHandler.java
+++ src/main/java/com/fivan/kalah/bot/handler/InitialStateHandler.java
@@ -1,22 +1,19 @@
 package com.fivan.kalah.bot.handler;
 
-import com.fivan.kalah.bot.*;
-import com.fivan.kalah.dto.BoardRepresentation;
-import com.fivan.kalah.entity.Lobby;
+import com.fivan.kalah.bot.Event;
+import com.fivan.kalah.bot.HandlingResult;
+import com.fivan.kalah.bot.State;
 import com.fivan.kalah.entity.Player;
-import com.fivan.kalah.service.GameService;
-import com.fivan.kalah.service.LobbyService;
 import com.fivan.kalah.service.PlayerService;
 import com.fivan.kalah.util.GameUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Handler(State.INITIAL)
@@ -26,15 +23,11 @@ public class InitialStateHandler implements StateHandler {
   public static final String START_COMMAND = "/start";
 
   private final PlayerService playerService;
-  private final LobbyService lobbyService;
-  private final GameService gameService;
-  private final KeyboardService keyboardService;
+  private final JoinGameHandler joinGameHandler;
 
   @Override
   public HandlingResult handle(Update update) {
-
     var botApiMethods = new ArrayList<BotApiMethod<?>>();
-    var actions = new ArrayList<LobbySendMessageAction>();
 
     Integer playerId = GameUtils.getUserIdFromMessage(update);
     if (playerService.getById(playerId).isEmpty()) {
@@ -46,45 +39,13 @@ public class InitialStateHandler implements StateHandler {
           .setChatId(playerId.longValue()));
     }
 
-    String[] splitMessage = update.getMessage().getText().split(" ");
-
-    if (splitMessage[0].equals(START_COMMAND) && splitMessage.length == 2) {
-      String potentialLobbyId = splitMessage[1];
-      try {
-        UUID lobbyId = UUID.fromString(potentialLobbyId);
-        Lobby lobby = lobbyService.findById(lobbyId)
-            .orElseThrow(() -> new IllegalArgumentException("Couldn't find lobby with id " + lobbyId));
-        Integer opponentPlayerId = lobby.getPlayerId();
-        BoardRepresentation board = gameService.createGame(opponentPlayerId, playerId);
-        lobbyService.addBoardId(lobbyId, board.getId());
-
-        SendMessage sendPlayerOneMessage = new SendMessage()
-            .setText("Your turn")
-            .setChatId(board.getPlayerOne().longValue())
-            .setReplyMarkup(keyboardService.preparePlayerOneButtons(board));
-
-        SendMessage sendPlayerTwoMessage =
-            new SendMessage()
-                .setText("Opponents turn")
-                .setChatId(board.getPlayerTwo().longValue())
-                .setReplyMarkup(keyboardService.preparePlayerTwoButtons(board));
-
-        LobbySendMessageAction playerOneAction = new LobbySendMessageAction(sendPlayerOneMessage, message ->
-            lobbyService.addPlayerOneMessageId(lobbyId, message.getMessageId())
-        );
-        LobbySendMessageAction playerTwoAction = new LobbySendMessageAction(sendPlayerTwoMessage, message ->
-            lobbyService.addPlayerTwoMessageId(lobbyId, message.getMessageId())
-        );
-        actions.addAll(List.of(playerOneAction, playerTwoAction));
-      } catch (IllegalArgumentException e) {
-        log.warn("{} is not correct UUID", potentialLobbyId, e);
-      }
-    }
+    Optional<HandlingResult> joinGameResults = joinGameHandler.handle(update);
+    joinGameResults.ifPresent(results -> botApiMethods.addAll(results.getMethods()));
 
     return HandlingResult.builder()
         .event(Event.TO_MENU)
         .methods(botApiMethods)
-        .actions(actions)
+        .actions(joinGameResults.map(HandlingResult::getActions).orElse(List.of()))
         .build();
   }
 }
