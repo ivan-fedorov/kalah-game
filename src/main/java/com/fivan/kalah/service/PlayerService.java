3f4f775e89717ddd8a76cda496fadbb9fb2ec1e8
Ivan Fedorov <11014428+ivan-fedorov@users.noreply.github.com>
2021-08-21 22:01:56 +0200

    Add top-10 button (#18)

--- src/main/java/com/fivan/kalah/service/PlayerService.java
+++ src/main/java/com/fivan/kalah/service/PlayerService.java
@@ -3,10 +3,13 @@ package com.fivan.kalah.service;
 import com.fivan.kalah.entity.Player;
 import com.fivan.kalah.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,9 @@ public class PlayerService {
 
   public boolean playersExistsById(List<Integer> players) {
     return players.stream().map(repository::findById).allMatch(Optional::isPresent);
+  }
+
+  public List<Player> findTopTenPlayers() {
+    return repository.findByOrderByRatingDesc(PageRequest.of(0, 10));
   }
 }
