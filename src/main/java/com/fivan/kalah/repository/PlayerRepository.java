a382d3218959d05c3c9fd733e4636c87b55d8b9a
Ivan Fedorov <11014428+ivan-fedorov@users.noreply.github.com>
2021-08-21 22:01:56 +0200

    Add top-10 button (#18)

--- src/main/java/com/fivan/kalah/repository/PlayerRepository.java
+++ src/main/java/com/fivan/kalah/repository/PlayerRepository.java
@@ -1,8 +1,15 @@
 package com.fivan.kalah.repository;
 
 import com.fivan.kalah.entity.Player;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PlayerRepository extends CrudRepository<Player, Integer> {}
+public interface PlayerRepository extends CrudRepository<Player, Integer> {
+
+  List<Player> findByOrderByRatingDesc(Pageable pageable);
+}
