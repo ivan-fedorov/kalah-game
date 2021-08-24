5beb0c324fbc85d9d7c90e72be5deec5760f94df
Ivan Fedorov <timegit13@gmail.com>
2021-08-24 19:52:37 +0200

    Store player state (#27)

--- src/main/java/com/fivan/kalah/bot/handler/DispatcherHandler.java
+++ src/main/java/com/fivan/kalah/bot/handler/DispatcherHandler.java
@@ -3,6 +3,8 @@ package com.fivan.kalah.bot.handler;
 import com.fivan.kalah.bot.Event;
 import com.fivan.kalah.bot.HandlingResult;
 import com.fivan.kalah.bot.State;
+import com.fivan.kalah.entity.PlayerState;
+import com.fivan.kalah.repository.PlayerStateRepository;
 import com.fivan.kalah.util.GameUtils;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -10,7 +12,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.romangr.simplestatemachine.StateMachine;
 import ru.romangr.simplestatemachine.StateMachineConfiguration;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -19,15 +24,17 @@ public class DispatcherHandler {
 
   private final MakeMoveCallbackHandler moveCallbackHandler;
   private final StateMachineConfiguration<State, Event> stateEventStateMachine;
-  private final Map<Integer, State> playerStateStorage = new HashMap<>();
+  private final PlayerStateRepository playerStateRepository;
   private final Map<State, StateHandler> handlersRoadMap;
   private final JoinGameHandler joinGameHandler;
 
   public DispatcherHandler(
       MakeMoveCallbackHandler moveCallbackHandler,
+      PlayerStateRepository playerStateRepository,
       List<StateHandler> stateHandlers,
       JoinGameHandler joinGameHandler) {
     this.moveCallbackHandler = moveCallbackHandler;
+    this.playerStateRepository = playerStateRepository;
     this.joinGameHandler = joinGameHandler;
     stateEventStateMachine =
         StateMachine.<State, Event>builder()
@@ -54,17 +61,17 @@ public class DispatcherHandler {
           .build();
     }
 
-    State playerState = playerStateStorage.get(userId);
+    Optional<PlayerState> playerStateOptional = playerStateRepository.findById(userId);
 
     // If player state is not present, we just call initial handler and add event to transitionsMap
-    if (playerState == null) {
+    if (playerStateOptional.isEmpty()) {
       HandlingResult result = handlersRoadMap.get(State.INITIAL).handle(update);
 
       StateMachine<State, Event> playerStateMachine =
           StateMachine.fromInitialState(stateEventStateMachine);
       State currentState = playerStateMachine.acceptEvent(result.getEvent()).newState();
 
-      playerStateStorage.put(userId, currentState);
+      playerStateRepository.save(new PlayerState(userId, currentState, null));
 
       return ActionsAndMethods.builder()
           .methods(addInitialMethodToCurrent(update, result.getMethods(), currentState))
@@ -81,13 +88,14 @@ public class DispatcherHandler {
           .build();
     }
 
+    State playerState = playerStateOptional.get().getState();
     HandlingResult result = handlersRoadMap.get(playerState).handle(update);
 
     StateMachine<State, Event> playerStateMachine =
         StateMachine.fromState(stateEventStateMachine, playerState);
     State newState = playerStateMachine.acceptEvent(result.getEvent()).newState();
 
-    playerStateStorage.put(userId, newState);
+    playerStateRepository.save(playerStateOptional.get().withState(newState));
 
     if (!playerState.equals(newState)) {
       return ActionsAndMethods.builder()
